a1298eed9c8cac6e9ec2e296bd2ff16eaafec41f
vtu24854-nithish <vtu24854@veltech.edu.in>
2026-04-14 21:51:32 +0530

    Time: 5 ms (65.82%), Space: 47.1 MB (46.08%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean destroyed = false;
+
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                if (stack.peek() < -a) {
+                    stack.pop();
+                } else if (stack.peek() == -a) {
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                stack.push(a);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
