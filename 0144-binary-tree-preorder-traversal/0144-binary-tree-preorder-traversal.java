066060dd2cd623d9a8e4f679ceee9bb4e08b32d0
vtu24854-nithish <vtu24854@veltech.edu.in>
2026-04-14 21:32:06 +0530

    Time: 0 ms (100%), Space: 43.1 MB (56.68%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        
+        if (root == null) return result;
+        
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+        
+        return result;
+    }
+}
