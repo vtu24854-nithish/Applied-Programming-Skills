654975b3833d0509c7865319372e0e72318cffec
vtu24854-nithish <vtu24854@veltech.edu.in>
2026-04-14 21:40:42 +0530

    Time: 4 ms (73.5%), Space: 49.9 MB (30.83%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        dfs(root, "", result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> result) {
+        if (node == null) return;
+
+        if (path.length() == 0) {
+            path = String.valueOf(node.val);
+        } else {
+            path = path + "->" + node.val;
+        }
+
+        if (node.left == null && node.right == null) {
+            result.add(path);
+            return;
+        }
+
+        dfs(node.left, path, result);
+        dfs(node.right, path, result);
+    }
+}
