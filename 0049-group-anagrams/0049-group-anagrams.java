79dca9193b18445cc3e933be0418be2dbb64632e
vtu24854-nithish <vtu24854@veltech.edu.in>
2026-05-09 22:50:40 +0530

    Time: 6 ms (99.52%), Space: 49.7 MB (59.98%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+
+            char[] arr = str.toCharArray();
+
+            Arrays.sort(arr);
+
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
